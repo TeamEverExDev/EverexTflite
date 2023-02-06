f50c01958e33af16ff13f356795dca4a176e7c1d
kim dong joo <50819258+redhotsixbull@users.noreply.github.com>
2023-02-06 16:52:48 +0900

    코드 정리

--- ios/Classes/SwiftEverexTflitePlugin.swift
+++ ios/Classes/SwiftEverexTflitePlugin.swift
@@ -41,14 +41,13 @@ public class SwiftEverexTflitePlugin: NSObject, FlutterPlugin {
             var options = Interpreter.Options()
             options.threadCount = 2
             
-            var coreMLOption = CoreMLDelegate.Options()
-            coreMLOption.enabledDevices = .all
+            //GPU 사용안됨
+            //var coreMLOption = CoreMLDelegate.Options()
+            //coreMLOption.enabledDevices = .all
             
-            let coreMLDelegate = CoreMLDelegate(options: coreMLOption)
-            let delegates : Delegate = coreMLDelegate ?? MetalDelegate()
-            
-
-            
+            //let coreMLDelegate = CoreMLDelegate(options: coreMLOption)
+            //let delegates : Delegate = coreMLDelegate ?? MetalDelegate()
+            let delegates : Delegate = MetalDelegate()
             
             do {
                 interpreter = try Interpreter(modelPath: modelPath ?? "", options: options, delegates: [delegates])
@@ -89,40 +88,12 @@ public class SwiftEverexTflitePlugin: NSObject, FlutterPlugin {
             let typedData : FlutterStandardTypedData = temp.first!
             
             let data = Data(typedData.data)
-            var myUInt8bytes: [UInt8] = data.toArray(type: UInt8.self)
+            let myUInt8bytes: [UInt8] = data.toArray(type: UInt8.self)
             
+            let resizeUint8Bytes : [UInt8] = resizeBgraImage(pixels: myUInt8bytes, width: 288, height: 352, targetSize: CGSize(width: 240, height: 320))
+            let rgb : [UInt8] = bgraToRgb(pixels: resizeUint8Bytes, width: 240, height: 320)
             
-            var rawPointer: UnsafeMutableRawPointer?
-            myUInt8bytes.withUnsafeMutableBytes { bufferPointer in
-                rawPointer = bufferPointer.baseAddress }
-            
-            var sourceBuffer = vImage_Buffer(
-                data: rawPointer,
-                height: vImagePixelCount(352),
-                width: vImagePixelCount(288),
-                rowBytes: 4 * 288)
-            
-            let destinationBytesPerRow = Constants.rgbPixelChannels * 240
-            
-            let destinationData = malloc(320 * destinationBytesPerRow)
-            
-            var destinationBuffer = vImage_Buffer(
-                data: destinationData,
-                height: vImagePixelCount(320),
-                width: vImagePixelCount(240),
-                rowBytes: destinationBytesPerRow)
-            
-            vImageConvert_BGRA8888toRGB888(&sourceBuffer, &destinationBuffer, UInt32(kvImageNoFlags))
-            
-            let imageByteData = Data(
-                bytes: destinationBuffer.data, count: destinationBuffer.rowBytes * 320)
-            
-            let imageBytes = [UInt8](imageByteData)
-            
-            //let resizeUint8Bytes : [UInt8] = resizeBgraImage(pixels: myUInt8bytes, width: 288, height: 352, targetSize: CGSize(width: 240, height: 320))
-            //let rgb : [UInt8] = bgraToRgb(pixels: resizeUint8Bytes, width: 240, height: 320)
-            
-            runPoseNet(on: imageBytes)
+            runPoseNet(on: rgb)
             result(true)
         case "outPut":
             result(positions)
@@ -154,7 +125,7 @@ public class SwiftEverexTflitePlugin: NSObject, FlutterPlugin {
             fatalError("Preprocessing failed")
         }
         
-       
+        
         inference(from: data)
         
         postprocess()
@@ -177,56 +148,24 @@ public class SwiftEverexTflitePlugin: NSObject, FlutterPlugin {
     }
     
     private func preprocess(of pixelBuffer: [UInt8]) -> Data? {
-        //let imageBytes = [UInt8](pixelBuffer)
-        let destinationBytesPerRow = Constants.rgbPixelChannels * 240
-        //
-        //        var data:[UInt8] = pixelBuffer
-        //
-        //        var sourceBuffer = vImage_Buffer(data: &data, height: vImagePixelCount(320), width: vImagePixelCount(240), rowBytes: 240*3)
-        //
-        //        guard let destinationData = malloc(320 * destinationBytesPerRow) else {
-        //            os_log("Error: out of memory", type: .error)
-        //            return nil
-        //        }
-        //
-        //        defer { free(destinationData) }
-        //
-        //        var destinationBuffer = vImage_Buffer(
-        //            data: destinationData,
-        //            height: vImagePixelCount(320),
-        //            width: vImagePixelCount(240),
-        //            rowBytes: destinationBytesPerRow)
-        //
-        //        vImageConvert_BGRA8888toRGB888(&sourceBuffer, &destinationBuffer, UInt32(kvImageNoFlags))
-        //
-        //        let imageByteData = Data(
-        //            bytes: destinationBuffer.data, count: destinationBuffer.rowBytes * 320)
-        //
-        //        let imageBytes = [UInt8](imageByteData)
         
+        let destinationBytesPerRow = Constants.rgbPixelChannels * 240
         
         
         
         var floats : [Float] = []
-        //var sum : Float = 0
+        
         floats.reserveCapacity(240 * 320 * Constants.rgbPixelChannels)
         for y in 0 ..< 320 {
             for x in 0 ..< 240 {
-           
+                
                 floats.append(Float(pixelBuffer[y * destinationBytesPerRow + x * 3]) - Constants.mean_R)
                 floats.append(Float(pixelBuffer[y * destinationBytesPerRow + x * 3 + 1]) - Constants.mean_G)
                 floats.append(Float(pixelBuffer[y * destinationBytesPerRow + x * 3 + 2]) - Constants.mean_B)
-//                sum = sum + (Float(pixelBuffer[y * destinationBytesPerRow + x * 3]) - Constants.mean_R ) + (Float(pixelBuffer[y * destinationBytesPerRow + x * 3 + 1]) - Constants.mean_G) + (Float(pixelBuffer[y * destinationBytesPerRow + x * 3 + 2]) - Constants.mean_B)
             }
         }
-        //print(sum)
-        
-        //return Data(buffer: UnsafeBufferPointer(start: floats, count: floats.count))
-        //var normailzeImage:[Float] = normalizeRGBImage(image: floats)
-        //Data(copyingBufferOf: floats)
         
         return Data(copyingBufferOf: floats)
-        //return Data(buffer: UnsafeBufferPointer(start: normailzeImage, count: normailzeImage.count))
     }
     
     func postprocess() {
@@ -234,19 +173,6 @@ public class SwiftEverexTflitePlugin: NSObject, FlutterPlugin {
         // Convert `Tensor` to `FlatArray`. As PoseNet is not quantized, convert them to Float type
         // `FlatArray`.
         let heats: FlatArray<Float32> = FlatArray<Float32>(tensor: heatsTensor!)
-        
-        
-//        let outputSize = (heatsTensor?.shape.dimensions.reduce(1, {x, y in x * y}))!
-//        let outputData =
-//              UnsafeMutableBufferPointer<Float32>.allocate(capacity: outputSize)
-//
-//        heatsTensor?.data.copyBytes(to: outputData)
-//
-        // MARK: Find position of each key point
-        // Finds the (row, col) locations of where the keypoints are most likely to be. The highest
-        // `heats[0, row, col, keypoint]` value, the more likely `keypoint` being located in (`row`,
-        // `col`).
-        //print(heats)
         
         positions = Array<Float>()
         
@@ -258,7 +184,7 @@ public class SwiftEverexTflitePlugin: NSObject, FlutterPlugin {
             for x in 0..<Model.output.width {
                 for y in 0..<Model.output.height {
                     let value = heats[0, y, x, i]
-                
+                    
                     if(value > max) {
                         max = value
                         maxX = x
@@ -266,17 +192,6 @@ public class SwiftEverexTflitePlugin: NSObject, FlutterPlugin {
                     }
                 }
             }
-            
-            
-            if(i==1) {
-                print("///////////")
-                print(maxX)
-                print(maxY)
-                print(max)
-            }
-         
-           
-            
             
             var maxXf:Float = Float(maxX)
             var maxYf:Float = Float(maxY)
@@ -300,33 +215,6 @@ public class SwiftEverexTflitePlugin: NSObject, FlutterPlugin {
             }
             positions?.insert(maxXf, at: i * 2 + 0)
             positions?.insert(maxYf, at: i * 2 + 1)
-            
-            // subpixel refine
-            //            if maxY >= 1 && maxY < Model.output.height-1 {
-            //                let diffX = heats[0,  maxY+1, maxX,i] - heats[0,  maxY-1, maxX,i]
-            //                if diffX > 0 {
-            //                    maxYf = Float(maxY) + 0.25
-            //                }else {
-            //                    maxYf = Float(maxY) - 0.25
-            //                }
-            //            }else{
-            //                maxYf = Float(maxY)
-            //            }
-            //
-            //            if maxX >= 1 && maxX < Model.output.width-1 {
-            //                let diffY = heats[0,  maxY, maxX+1, i] - heats[0,  maxY, maxX-1,i]
-            //                if diffY > 0 {
-            //                    maxXf = Float(maxX) + 0.25
-            //                }else {
-            //                    maxXf = Float(maxX) - 0.25
-            //                }
-            //            }else{
-            //                maxXf = Float(maxX)
-            //            }
-            //
-            //
-            //            positions?.insert(maxXf, at: i * 2 + 0)
-            //            positions?.insert(maxYf, at: i * 2 + 1)
         }
     }
 }
@@ -377,10 +265,10 @@ func resizeBgraImage(pixels: [UInt8], width: Int, height: Int, targetSize: CGSiz
 func normalizeRGBImage(image: [Float]) -> [Float] {
     let maxValue = image.max()!
     let minValue = image.min()!
-
+    
     let normalizedImage = image.map { (value) -> Float in
         return (value - minValue) / (maxValue - minValue)
     }
-
+    
     return normalizedImage
 }
